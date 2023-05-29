cc87320e162347e75fe8da6e9ecd0e7f6febc7e6
mduem <m.duemel@gmx.de>
2023-05-29 21:22:54 +0200

    newest version

--- space_invaders/MauriceGYMdeepLearningAgentCartPole.py
+++ space_invaders/MauriceGYMdeepLearningAgentCartPole.py
@@ -4,25 +4,27 @@ import random
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.backend import clear_session
 import tensorflow as tf
 from collections import deque
+import gc
 
 class DQNAgent:
     def __init__(self, state_size, action_size):
         self.state_size = state_size
         self.action_size = action_size
-        self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
+        self.memory = deque(maxlen=10000)
+        self.gamma = 0.65    # discount rate
         self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
+        self.epsilon_min = 0.05
         self.epsilon_decay = 0.999
-        self.learning_rate = 0.001
+        self.learning_rate = 0.0001
         self.model = self._build_model()
 
     def _build_model(self):
         # Neural Net for Deep-Q learning Model
         model = Sequential()
-        model.add(Dense(24, input_dim=self.state_size, activation='relu'))
+        model.add(Dense(48, input_dim=self.state_size, activation='relu'))
         model.add(Dense(24, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(learning_rate=self.learning_rate))
@@ -35,7 +37,8 @@ class DQNAgent:
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
         act_values = self.model.predict(state, verbose=0)
-        return np.argmax(act_values[0])  # returns action
+        return np.argmax(act_values[0])
+
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
@@ -47,6 +50,9 @@ class DQNAgent:
                 Q_future  = self.model.predict(next_state, verbose=0)[0]
                 target[0][action] = reward + self.gamma * np.amax(Q_future)
             self.model.fit(state, target, epochs=1, verbose=0)
+
+        self.memory = deque(maxlen=self.memory.maxlen)
+
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -61,8 +67,8 @@ def main():
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    batch_size = 16
-    episodes = 10
+    batch_size = 64
+    episodes = 5000
 
     if not play:
         for e in range(episodes):
@@ -70,6 +76,8 @@ def main():
             state, _ = env.reset()
             state = np.reshape(state, [1, state_size])
             for step in range(500):
+                if step == 499:
+                    print("STEPS MAX")
                 #env.render()
                 action = agent.act(state)
                 next_state, reward, terminated, truncated, _ = env.step(action)
@@ -82,9 +90,11 @@ def main():
                 if len(agent.memory) > batch_size and (step % 5 == 0):
                     agent.replay(batch_size)
             if e % 25 == 0:
+                gc.collect()
+                clear_session()
                 agent.model.save_weights('model_weights_episode_{}.h5'.format(e))
     else:
-        weights_filename = 'weights_200.h5'
+        weights_filename = 'weights_1275.h5'
         agent.model.load_weights(weights_filename)
         for e in range(episodes):
             state, _ = env.reset()
